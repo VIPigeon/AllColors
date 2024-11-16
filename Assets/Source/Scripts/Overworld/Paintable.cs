af8d29f7a9066a9e8d933d6f202dd5626b555caa
BurnedTuner <ainol0912@gmail.com>
2024-11-16 16:07:37 +0300

    Paintable interacts with scene

--- Assets/Source/Scripts/Overworld/Paintable.cs
+++ Assets/Source/Scripts/Overworld/Paintable.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,9 +6,26 @@ using UnityEngine;
 public class Paintable : MonoBehaviour
 {
     [SerializeField] private SpriteRenderer _renderer;
+    [SerializeField] private Color _requiredColor;
+    [SerializeField] private float _requiredColorThreshold;
+    [SerializeField] private GameObject[] _whatToActivate;
+    [SerializeField] private GameObject[] _whatToDeactivate;
 
     public void ApplyCard(Card card)
     {
         _renderer.color = card.Color;
+        Debug.Log(FindObjectOfType<ColorPickerDeck>().ColorDistance(_requiredColor, card.Color));
+        if (FindObjectOfType<ColorPickerDeck>().ColorDistance(_requiredColor, card.Color) < _requiredColorThreshold)
+        {
+            DoEffect();
+        }
+    }
+
+    protected virtual void DoEffect()
+    {
+        foreach (GameObject item in _whatToActivate)
+            item.SetActive(true);
+        foreach (GameObject item in _whatToDeactivate)
+            item.SetActive(false);
     }
 }
